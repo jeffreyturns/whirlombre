80e6c2a8396ce92d03d279fac2dc3111d425bb0c
Jeffrey Turns <51068771+jeffreyturns@users.noreply.github.com>
2024-01-03 07:02:12 +0200

    feat(blog): new post view layout

--- website/layouts/blog.vue
+++ website/layouts/blog.vue
@@ -4,17 +4,6 @@ import DefaultLayout from '~/layouts/default.vue'
 
 <template>
   <DefaultLayout>
-    <div class="pb-16 pt-8">
-      <div class="mx-auto max-w-6xl px-4 lg:px-8 sm:px-6">
-        <BlogBackAction />
-        <div class="px-4 md:px-8 sm:px-6">
-          <div class="mx-auto max-w-3xl">
-            <main>
-              <slot />
-            </main>
-          </div>
-        </div>
-      </div>
-    </div>
+    <slot />
   </DefaultLayout>
 </template>
