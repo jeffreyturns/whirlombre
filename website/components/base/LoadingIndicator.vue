d56c5061d123c9db3e83c8192b5eec01906a56e3
Jeffrey Turns <51068771+jeffreyturns@users.noreply.github.com>
2024-01-17 13:52:47 +0200

    chore: add bg color to loading indicator

--- website/components/base/LoadingIndicator.vue
+++ website/components/base/LoadingIndicator.vue
@@ -3,7 +3,7 @@ const { isLoading, progress } = useLoadingIndicator()
 </script>
 
 <template>
-    <div class="fixed z-99 top-0 left-0 right-0 h-[3px]" v-if="isLoading">
-    <div class="h-full bg-accent-500 transition-all" :style="{ width: `${progress}%` }"></div>
+    <div class="fixed z-99 top-0 left-0 right-0 h-[6px] rounded-wl-small bg-accent-500/25" v-if="isLoading">
+    <div class="h-full bg-accent-500 transition-all rounded-wl-small" :style="{ width: `${progress}%` }" />
   </div>
 </template>
